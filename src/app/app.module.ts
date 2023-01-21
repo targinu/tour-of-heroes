2d398833be5cb42f11b943b89d4d2b02993d8a42
targinu <targino26@outlook.com>
2023-01-21 10:18:30 -0300

    Mostrando Mensagens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,26 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MessagesComponent } from './messages/messages.component';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+  ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatButtonModule,
     FlexLayoutModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
