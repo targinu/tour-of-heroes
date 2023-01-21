5ecbf1d58ae5000b8da62549ffdc911a7f868b0e
targinu <targino26@outlook.com>
2023-01-20 23:05:48 -0300

    Detalhe do Hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,11 +8,6 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Libélula',
-  };
-
   heroes = HEROES;
   selectedHero?: Hero;
 
