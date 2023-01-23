e2608326b6717ac425adba0a9d7f1f4fc0f1d70b
targinu <targino26@outlook.com>
2023-01-23 12:29:34 -0300

    Core Module

---
+++ src/app/core/components/toolbar/toolbar.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { MenuItem } from '../../models/menu-item.model';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.css'],
+})
+export class ToolbarComponent {
+  @Input() title = '';
+  @Input() menuItems: MenuItem[] = [];
+}
